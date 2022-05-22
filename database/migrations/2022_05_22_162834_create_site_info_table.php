abf9cd3ca49678d92a9b70c03742d68aec02ae09
chhotuthakur <npsdahiya@gmail.com>
2022-05-22 22:19:13 +0530

    updated routes, added controllers and connected to database

---
+++ database/migrations/2022_05_22_162834_create_site_info_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSiteInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('site_info', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('subtitle');
+            $table->string('con_mail');
+            $table->string('con_num');
+            $table->string('reg_num');
+            $table->string('copyr');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('site_info');
+    }
+}
